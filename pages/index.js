c03d0d975aea464e8cc48b2599a75ee68f466797
mark-p7 <markdeguzman1142@gmail.com>
2022-09-09 15:35:04 -0700

    repush

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js w/ Firebase Client-Side</title>
+        <title>Next.js w/ Firebase Client-Side </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
